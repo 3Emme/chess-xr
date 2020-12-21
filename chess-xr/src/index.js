9e7a81ddcea6835cb72b15cd5d2bfe0af59109f7
Emme Buentiempo <emmettbuentiempo@gmail.com>
2020-12-20 22:23:39 -0800

    attempt to usegltfjsx to render chessboard but not sure if compatible with reactxr

--- chess-xr/src/index.js
+++ chess-xr/src/index.js
@@ -1,35 +1,36 @@
 import { useLoader } from "react-three-fiber"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import ReactDOM from 'react-dom'
-import React, { useState, useRef } from 'react'
+import React, { Suspense, useState, useRef } from 'react'
 import { OrbitControls, Box } from 'drei'
 import { VRCanvas, DefaultXRControllers, Hover } from 'react-xr'
+import Model from './Chessboard'
 import './styles.css'
 
-function Chessboard() {
-  // Load the gltf file
-  const { nodes, materials } = useLoader(GLTFLoader, "/chessboard.gltf")
-  // Fetch some reactive state
-  const model = useRef()
+// function Chessboard() {
+//   // Load the gltf file
+//   const { nodes, materials } = useLoader(GLTFLoader, "/chessboard.gltf")
+//   // Fetch some reactive state
+//   const model = useRef()
 
-  return (
-    <mesh dispose={null} scale={[0.1, 0.1, 0.1]}>
-      <group ref={model} position={[-0.05, 0.37, 0.3]} scale={[0.15, 0.15, 0.15]}>
-        <group rotation={[1.88, -0.35, 2.32]} scale={[2.97, 2.97, 2.97]}>
-          <primitive object={nodes.Bone} />
-          <skinnedMesh castShadow receiveShadow material={materials.glove} material-roughness={1} geometry={nodes.arm.geometry} skeleton={nodes.arm.skeleton} />
-        </group>
-        <group rotation={[0, -0.04, 0]} scale={[141.94, 141.94, 141.94]}>
-          <mesh castShadow receiveShadow material={materials.wood} geometry={nodes.mesh_0.geometry} />
-        </group>
-      </group>
-    </mesh>
-  )
-}
+//   return (
+//     <mesh dispose={null} scale={[0.1, 0.1, 0.1]}>
+//       <group ref={model} position={[-0.05, 0.37, 0.3]} scale={[0.15, 0.15, 0.15]}>
+//         <group rotation={[1.88, -0.35, 2.32]} scale={[2.97, 2.97, 2.97]}>
+//           <primitive object={nodes.Bone} />
+//           <skinnedMesh castShadow receiveShadow material={materials.glove} material-roughness={1} geometry={nodes.arm.geometry} skeleton={nodes.arm.skeleton} />
+//         </group>
+//         <group rotation={[0, -0.04, 0]} scale={[141.94, 141.94, 141.94]}>
+//           <mesh castShadow receiveShadow material={materials.wood} geometry={nodes.mesh_0.geometry} />
+//         </group>
+//       </group>
+//     </mesh>
+//   )
+// }
 
 const App = () => {
-  const [isHovered, setIsHovered] = useState(false)
-  const color = isHovered ? 'blue' : '#e23'
+  // const [isHovered, setIsHovered] = useState(false)
+  // const color = isHovered ? 'blue' : '#e23'
 
   return (
     <VRCanvas>
@@ -38,13 +39,16 @@ const App = () => {
 
       <OrbitControls />
 
-      <Hover onChange={setIsHovered}>
+      {/* <Hover onChange={setIsHovered}>
         <Box position={[0, 0.8, -1]} scale={[0.3, 0.3, 0.3]}>
           <meshStandardMaterial color={color} />
         </Box>
-      </Hover>
-      {/* <Chessboard position={[5, 0.8, -1]} scale={[0.3, 0.3, 0.3]}>
+      </Hover> */}
+      {/* <Chessboard position={[5, 0.8, -2]} scale={[1, 1, 1]}>
       </Chessboard> */}
+      
+        <Model />
+      
       <DefaultXRControllers />
     </VRCanvas>
   )
